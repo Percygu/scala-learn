bb2bf289d9f9e984222d5554e99c386864b215c4
Clarkgu <peng.gu@shopee.com>
2018-11-28 17:58:09 +0800

    add absrtract class

---
+++ extends_learn/.#extends_learn.scala
@@ -0,0 +1,1 @@
+peng.gu@gupengdeMacBook-Pro.local.83246
--- extends_learn/extends_learn.scala
+++ extends_learn/extends_learn.scala
@@ -17,6 +17,18 @@ class SecretAgent(name:String,age:Int) extends Person(name,age){
 
 }
 
+//抽象类，类中含有全部或部分没有被完整定义的方法必须以abstract关键字表示为抽象类
+abstract class Person1(val name: String,val privateId:Int){
+  def id: Int   //声明无参函数id，返回Int类型，抽象方法
+  val age: Int   //没有被初始化，这是一个带有抽象的getter方法的抽象字段
+  var phone: String   //没有被初始化，这是一个带有抽象的setter和getter方法的抽象字段
+}
+
+class Employee1(name:String,privateId:Int) extends Person1(name,privateId){
+  def id = privateId  //子类重写超类的抽象方法不需要加override关键字
+  val age = 16           //重写抽象超类的抽象字段
+  var phone = "13382938220"    //重写抽象超类的抽象字段
+}
 
 object Test{
   def main(args: Array[String]) :Unit = {
@@ -25,5 +37,8 @@ object Test{
     println(employee.getName)
     val agent = new SecretAgent("lisi",20)
     println(agent.getName)  //打印Hello lisi
+    val p: Person1 = new Employee1("wangwu",1)     //用超类Person1声明对象p,用子类Employee1实现对象
+    println(p.age)
+    println(p.id)
   }
 }
